7013dfe38f6632c87ab9b17c0c54fef3485b096b
AnnaDavydenko <annadavydenko2544737@gmail.com>
2020-10-03 17:31:41 +0300

    feat: add carbon-dating, what-season

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,5 +2,10 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  throw new CustomError('Not implemented');
+  if( sampleActivity !=="string" || sampleActivity === undefined || Number(sampleActivity)!=="number" || sampleActivity=== NaN){
+    return false;
+  } else {
+    return Math.ceil(Math.log((MODERN_ACTIVITY/Number(sampleActivity)))/(Math.log(2)/HALF_LIFE_PERIOD));
+  }
 };
+
--- src/what-season.js
+++ src/what-season.js
@@ -6,44 +6,53 @@ module.exports = function getSeason(date) {
 
   var isDate = true;
 
-  try {
-    date = new Date(date);
-    if (!date || date.getMonth || !date.getYear) {
+  if (date instanceof Date && !isNaN(date)) {
+    try {
+      date = new Date(date);
+      if (!date || !date.getMonth || !date.getYear) {
+        isDate = false;
+      }
+    } catch (e) {
       isDate = false;
     }
-  } catch {
-    isDate = false;
+
+    if (!isDate) {
+      throw new Error('enter the correct argument');
+    }
+
+    const month = date.getMonth();
+
+    let season = '';
+
+    switch (month) {
+      case 11:
+      case 0:
+      case 1:
+        season = 'winter';
+        break;
+      case 2:
+      case 3:
+      case 4:
+        season = 'spring';
+        break;
+      case 5:
+      case 6:
+      case 7:
+        season = 'summer';
+        break;
+      case 8:
+      case 9:
+      case 10:
+        season = 'autumn';
+        break;
+    }
+
+    if (!season) {
+      throw new Error('enter the correct argument');
+    }
+
+    return season;
+  } else {
+    throw new Error();
   }
-
-  if (!isDate) {
-    throw new Error('enter the correct argument');
-  }
-
-  const month = date.getMonth();
-
-  let season = '';
-
-  switch (month) {
-    case 11:
-    case 0:
-    case 1:
-      season = 'winter';
-      break;
-    case 2:
-    case 3:
-    case 4:
-      season = 'spring';
-      break;
-    case 5:
-    case 6:
-    case 7:
-      season = 'summer';
-      break;
-    case 8:
-    case 9:
-    case 10:
-      season = 'autumn';
-      break;
-  }
-  return season;
-};
+}
